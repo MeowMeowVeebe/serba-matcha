1c602fa690001b29c30b341674d60f12a33c2754
Your Name <you@example.com>
2026-01-17 12:37:27 +0700

    Add homepage layout

---
+++ app/components/contact.tsx
@@ -0,0 +1,11 @@
+export default function contacepage() {
+  return (
+
+
+    <div className="min-h-screen bg-zinc-50 font-sans dark:bg-black">
+
+      <h1 className="text-4xl font-bold text-center">This Contact Page</h1>
+
+      </div>
+  );
+}
